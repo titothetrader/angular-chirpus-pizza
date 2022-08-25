2442b174632cb470a0ae7b8f2b2b751167357386
Tito Sanchez <99434691+titothetrader@users.noreply.github.com>
2022-08-24 21:58:36 -0400

    init commit
    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { MenuItem } from '../interfaces/menu-item';
+
+import {
+  faCoffee,
+  faPizzaSlice,
+  faDrumstickBite,
+  faBreadSlice,
+  faLeaf,
+  faIceCream,
+  faBeerMugEmpty,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-chirpus-pizza';
+  title = 'Chirpus Pizza';
+  faCoffee = faCoffee;
+
+  menuItems: MenuItem[] = [
+    {
+      name: 'Chicken Fingers',
+      category: 'Dinner',
+      price: 11.99,
+      icon: faDrumstickBite,
+    },
+
+    {
+      name: 'Pizza',
+      category: 'Dinner',
+      price: 11.99,
+      icon: faPizzaSlice,
+    },
+
+    {
+      name: 'Breadsticks',
+      category: 'Sides',
+      price: 4.99,
+      icon: faBreadSlice,
+    },
+
+    {
+      name: 'Caesar Salad',
+      category: 'Salads',
+      price: 5.99,
+      icon: faLeaf,
+    },
+
+    {
+      name: 'Ice Cream',
+      category: 'Dessert',
+      price: 8.99,
+      icon: faIceCream,
+    },
+
+    {
+      name: 'Beer',
+      category: 'Drink',
+      price: 3.99,
+      icon: faBeerMugEmpty,
+    },
+  ];
 }
---
+++ src/interfaces/menu-item.ts
@@ -0,0 +1,6 @@
+export interface MenuItem {
+  name: string;
+  category: string;
+  price: number;
+  icon: any;
+}
